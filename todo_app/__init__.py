3ca2eee6205a44dbb1255071fdccd70d9b6bd72a
Kevin Keller <kellerkev@gmail.com>
2024-11-04 11:46:16 +0100

    Update __init__.py

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+from snowflake.sqlalchemy import URL
 
 # Initialize the extension SQLAlchemy
 db = SQLAlchemy()
